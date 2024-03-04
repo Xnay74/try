7919cd8fe01681cf57bc80832636cd0f8ffa483f
Xnay74 <145762920+Xnay74@users.noreply.github.com>
2024-03-04 14:30:38 +0700

    Add files via upload

---
+++ PBO/Cat.java
@@ -0,0 +1,20 @@
+public class Cat {
+    public String nama = "Recky";
+    public int umur = 2;
+    public String gender = "cowok";
+    public String makanan = "whiskas";
+    public String warna = "hitam putih";
+    public void iscathungry(){
+        System.out.println(this.nama + " is hungry");
+    }
+    public void iscatitred(){
+        System.out.println(this.nama + " go to sleep");
+    }
+    public void iscatangry(){
+        System.out.println(this.nama + " want to eat");
+    }
+}
+
+
+
+
---
+++ PBO/MainDriver.java
@@ -0,0 +1,8 @@
+public class MainDriver {
+    public static void main(String[] args) {
+        Cat Cat1 = new Cat();
+        Cat1.iscathungry();
+        Cat1.iscatitred();
+        Cat1.iscatangry();
+    }
+}
